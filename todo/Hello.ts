28bb3423a4ef890034163369af056f2e41cab435
Sean Malloy <techfan321@gmail.com>
2021-11-29 19:10:20 -0500

    Removed extra TS file

--- todo/Hello.ts
+++
@@ -1,7 +0,0 @@
-console.log("Hello from TypeScript!")
-
-function doAdd(a: number, b: number) : number {
-    return a + b
-}
-
-console.log(`a + b = ${doAdd(1, 2)}`)
--- todo/HelloTS.ts
+++ todo/HelloTS.ts
@@ -1,6 +1,6 @@
 console.log("Hello from TypeScript!")
 
-function doAdd(a: number, b: number) {
+function doAdd(a: number, b: number) : number {
     return a + b
 }
 
