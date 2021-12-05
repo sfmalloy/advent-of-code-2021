d0ff328032402f33cafc9233c2194a299bbd061f
Sean Malloy <techfan321@gmail.com>
2021-12-05 13:33:18 -0500

    [Day 5] Optimization and cleaning

--- Day05.rb
+++ Day05.rb
@@ -1,3 +1,7 @@
+require 'benchmark'
+
+Point = Struct.new(:x, :y)
+
 def bounds(a, b)
     start = 0
     finish = 0
@@ -7,76 +11,87 @@ def bounds(a, b)
     return b, a
 end
 
-Point = Struct.new(:x, :y)
+def solve
+    no_diag = Hash.new(0)
+    diag = Hash.new(0)
 
-no_diag = Hash.new(0)
-diag = Hash.new(0)
+    File.readlines("inputs/Day05.in").each do |line|
+        p1, p2 = line.split(" -> ")
+        x1, y1 = p1.split(",").map(&:to_i)
+        x2, y2 = p2.split(",").map(&:to_i)
 
-File.readlines("inputs/Day05.in").each do |line|
-    p1, p2 = line.split(" -> ")
-    x1, y1 = p1.split(",").map(&:to_i)
-    x2, y2 = p2.split(",").map(&:to_i)
+        dx = 0
+        dy = 0
+        x = x_end = y = y_end = 0
+        is_diag = false
+        if y1 == y2 then
+            dx = 1
+            y = y_end = y1
+            x, x_end = bounds(x1, x2)
+        elsif x1 == x2 then
+            dy = 1
+            x = x_end = x1
+            y, y_end = bounds(y1, y2)
+        elsif x1 > x2 then
+            dy = y2 < y1 ? 1 : -1
+            dx = 1
+            x = x2
+            y = y2
+            x_end = x1
+            y_end = y1
+            is_diag = true
+        else
+            dy = y1 < y2 ? 1 : -1
+            dx = 1
+            x = x1
+            y = y1
+            x_end = x2
+            y_end = y1
+            is_diag = true
+        end
 
-    dx = 0
-    dy = 0
-    x = x_end = y = y_end = 0
-    is_diag = false
-    if y1 == y2 then
-        dx = 1
-        y = y_end = y1
-        x, x_end = bounds(x1, x2)
-    elsif x1 == x2 then
-        dy = 1
-        x = x_end = x1
-        y, y_end = bounds(y1, y2)
-    elsif x1 > x2 then
-        dy = y2 < y1 ? 1 : -1
-        dx = 1
-        x = x2
-        y = y2
-        x_end = x1
-        y_end = y1
-        is_diag = true
-    else
-        dy = y1 < y2 ? 1 : -1
-        dx = 1
-        x = x1
-        y = y1
-        x_end = x2
-        y_end = y1
-        is_diag = true
+        if not is_diag then
+            while x <= x_end and y <= y_end do
+                pt = Point.new(x, y)
+                no_diag[pt] += 1
+                diag[pt] += 1
+                y += dy
+                x += dx
+            end
+        else
+            while x <= x_end do
+                pt = Point.new(x, y)
+                diag[pt] += 1
+                y += dy
+                x += dx
+            end
+        end
     end
 
-    if not is_diag then
-        while x <= x_end and y <= y_end do
-            pt = Point.new(x, y)
-            no_diag[pt] += 1
-            diag[pt] += 1
-            y += dy
-            x += dx
-        end
-    else
-        while x <= x_end do
-            pt = Point.new(x, y)
-            diag[pt] += 1
-            y += dy
-            x += dx
+    part1 = 0
+    no_diag.each do |pt, count|
+        if count > 1 then
+            part1 += 1
         end
     end
+
+    part2 = 0
+    diag.each do |pt, count|
+        if count > 1 then
+            part2 += 1
+        end
+    end
+
+    return part1, part2
 end
 
 part1 = 0
-no_diag.each do |pt, count|
-    if count > 1 then
-        part1 += 1
-    end
-end
-puts part1
-
 part2 = 0
-diag.each do |pt, count|
-    if count > 1 then
-        part2 += 1
-    end
-end
+time = Benchmark.realtime {
+    part1, part2 = solve()
+}
+
+puts part1
 puts part2
+time = sprintf("%.3f ms", time * 1000)
+puts time
